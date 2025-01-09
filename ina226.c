1e7113a2566fd04a5a88d2b827077341a137ff40
mao <maokai1905@gmail.com>
2025-01-09 19:02:42 +0800

    Implement ina226 related functions.

--- ina226.c
+++ ina226.c
@@ -1,2 +1,91 @@
 
 #include "ina226.h"
+
+#define SHUNT_VOLTAGE_LSB 2.5f /*uV*/
+#define BUS_VOLTAGE_LSB 1.25f  /*mV*/
+
+static inline int ina226_reg_mask_value(ina226_chip *chip, uint8_t reg,
+                                        uint16_t mask, uint16_t val) {
+  int ret;
+  uint16_t rv = 0;
+  if ((ret = chip->ops->read(chip->slave_addr, reg, (uint8_t *)&rv, 2)) != 0) {
+    return ret;
+  }
+
+  rv &= ~mask;
+  rv |= val;
+
+  if ((ret = chip->ops->write(chip->slave_addr, reg, (uint8_t *)&rv, 2)) != 0) {
+    return ret;
+  }
+  return ret;
+}
+int ina226_read_reg(ina226_chip *chip, uint8_t reg, uint16_t *val) {
+  uint8_t buf[2];
+  int ret = chip->ops->read(chip->slave_addr, reg, buf, 2);
+  *val = buf[1] | (buf[0] << 8);
+  return ret;
+}
+
+int ina226_write_reg(ina226_chip *chip, uint8_t reg, uint16_t val) {
+  uint8_t buf[] = {val >> 8, val & 0xff};
+  return chip->ops->write(chip->slave_addr, reg, buf, 2);
+}
+
+void ina226_init(ina226_chip *chip, uint8_t addr, const ina226_i2c_ops *ops) {
+  chip->slave_addr = addr;
+  chip->ops = ops;
+}
+
+int ina226_get_shunt_voltage(ina226_chip *chip, int16_t *vol) {
+  *vol = 0;
+  int ret = ina226_read_reg(chip, INA226_REG_SHUNT_VOLTAGE, (uint16_t *)vol);
+  *vol = (int16_t)(SHUNT_VOLTAGE_LSB * (*vol)); // uV
+  return ret;
+}
+
+int ina226_get_bus_voltage(ina226_chip *chip, int16_t *vbus) {
+  *vbus = 0;
+  int ret = ina226_read_reg(chip, INA226_REG_BUS_VOLTAGE, (uint16_t *)vbus);
+  *vbus = (int16_t)(BUS_VOLTAGE_LSB * (*vbus));
+
+  return ret;
+}
+
+// cur mA, shunt_res mR
+void ina226_set_calibration(ina226_chip *chip, uint16_t cur,
+                            uint16_t shunt_res) {
+  uint16_t lsb = (uint16_t)((((uint32_t)cur) * 1000) / (1 << 15)); // uA
+  if (lsb < 250) {
+    chip->current_lsb = 250;
+  } else if (lsb < 500) {
+    chip->current_lsb = 500;
+  } else if (lsb < 750) {
+    chip->current_lsb = 750;
+  } else if (lsb < 1000) {
+    chip->current_lsb = 1000;
+  } else if (lsb < 1500) {
+    chip->current_lsb = 1500;
+  } else {
+    chip->current_lsb = (lsb % 1000 + 1) * 1000;
+  }
+  // cal=0.00512/(lsb(A)*shunt_res(R))
+  uint16_t cal = 5120000 / (chip->current_lsb * shunt_res);
+  ina226_write_reg(chip, INA226_REG_CALIBRATION, cal);
+}
+
+int ina226_get_power(ina226_chip *chip, uint16_t *power) {
+  uint16_t p;
+  ina226_read_reg(chip, INA226_REG_POWER, &p);
+
+  *power = (p * (chip->current_lsb * 25)) / 1000; // mW
+
+  return 0;
+}
+
+int ina226_get_current(ina226_chip *chip, int16_t *cur) {
+  int16_t i;
+  ina226_read_reg(chip, INA226_REG_CURRENT, (uint16_t *)&i);
+  *cur = (i * chip->current_lsb) / 1000;
+  return 0;
+}
--- ina226.h
+++ ina226.h
@@ -1,4 +1,66 @@
 #ifndef __INA226_H
 #define __INA226_H
 
+#include <stdint.h>
+
+typedef enum {
+    INA226_REG_CONFIGURATION = 0x00,
+    INA226_REG_SHUNT_VOLTAGE = 0x01,
+    INA226_REG_BUS_VOLTAGE = 0x02,
+    INA226_REG_POWER = 0x03,
+    INA226_REG_CURRENT = 0x04,
+    INA226_REG_CALIBRATION = 0x05,
+    INA226_REG_MASK = 0x06,
+    INA226_REG_ALERT_LIMIT = 0x07,
+    INA226_REG_MANUFACTURER_ID = 0xFE,
+    INA226_REG_DIE_ID = 0xFF,
+} ina266_reg_e;
+
+#define INA226_CONF_MODE_MASK 0x7
+#define INA226_CONF_VSHCT_MASK 0x7 << 3
+#define INA226_CONF_VBUSCT_MASK 0x7 << 6
+#define INA226_CONF_AVG_MASK 0x7 << 9
+#define INA226_CONF_RST_MASK 0x01 << 15
+
+#define INA226_MAKE_CONF_REG(_mode, _vshct, _vbusct, _avg)                     \
+  (((_mode) & 0x7) | (((_vshct) & 0x7) << 3) | (((_vbusct) & 0x7) << 6) |      \
+   (((_avg) & 0x7) << 9) | (0x04 << 12) | (((_rst) & 0x1) << 15))
+
+#define INA226_GET_CONF_MODE(_c) ((_c) & INA226_CONF_MODE_MASK)
+#define INA226_GET_CONF_VSHCT(_c) (((_c) & INA226_CONF_VSHCT_MASK) >> 3)
+#define INA226_GET_CONF_VBUSCT(_c) (((_c) & INA226_CONF_VBUSCT_MASK) >> 6)
+#define INA226_GET_CONF_AVG(_c) (((_c) & INA226_CONF_AVG_MASK) >> 9)
+#define INA226_GET_CONF_RST(_c) (((_c) & INA226_CONF_RST_MASK) >> 15)
+
+typedef struct ina226_i2c_ops {
+    int8_t (*read)(uint8_t salve_addr, uint8_t reg, uint8_t *buf, uint16_t len);
+
+    int8_t (*write)(uint8_t salve_addr, uint8_t reg, const uint8_t *buf,
+                    uint16_t len);
+} ina226_i2c_ops;
+
+typedef struct ina226_chip {
+    const ina226_i2c_ops *ops;
+    uint8_t slave_addr;
+    uint16_t current_lsb;//uA
+} ina226_chip;
+
+void ina226_init(ina226_chip *chip, uint8_t addr, const ina226_i2c_ops *ops);
+
+int ina226_read_reg(ina226_chip *chip, uint8_t reg, uint16_t *val);
+
+int ina226_write_reg(ina226_chip *chip, uint8_t reg, uint16_t val);
+
+
+void ina226_set_calibration(ina226_chip *chip, uint16_t cur,
+                            uint16_t shunt_res);
+
+int ina226_get_shunt_voltage(ina226_chip *chip, int16_t *vol);
+
+int ina226_get_bus_voltage(ina226_chip *chip, int16_t *vbus);
+
+int ina226_get_power(ina226_chip *chip, uint16_t *power);
+
+int ina226_get_current(ina226_chip *chip, int16_t *cur);
+
 #endif
